d91b86b4e1a93448790899a3f01e1165421c1b6d
Yukino Song <nutosservice@gmail.com>
2024-10-31 17:43:41 +0800

    Revert "build(deps): migrate from curl to curl-winssl on Windows (#3246)"
    This reverts commit 9cc8bc8f62d5dfd6236d0b8ec44b6b9978bcbe87, should resolve #91

--- .codeql-prebuild-cpp-Windows.sh
+++ .codeql-prebuild-cpp-Windows.sh
@@ -2,35 +2,43 @@
 set -e
 
 # update pacman
-pacman --noconfirm -Syu
+pacman --noconfirm -Suy
+
+# install wget
+pacman --noconfirm -S \
+  wget
+
+# download working curl
+wget https://repo.msys2.org/mingw/ucrt64/mingw-w64-ucrt-x86_64-curl-8.8.0-1-any.pkg.tar.zst
 
 # install dependencies
-dependencies=(
-  "git"
-  "mingw-w64-ucrt-x86_64-boost"
-  "mingw-w64-ucrt-x86_64-cmake"
-  "mingw-w64-ucrt-x86_64-cppwinrt"
-  "mingw-w64-ucrt-x86_64-curl-winssl"
-  "mingw-w64-ucrt-x86_64-miniupnpc"
-  "mingw-w64-ucrt-x86_64-nlohmann-json"
-  "mingw-w64-ucrt-x86_64-nodejs"
-  "mingw-w64-ucrt-x86_64-nsis"
-  "mingw-w64-ucrt-x86_64-onevpl"
-  "mingw-w64-ucrt-x86_64-openssl"
-  "mingw-w64-ucrt-x86_64-opus"
-  "mingw-w64-ucrt-x86_64-toolchain"
-)
-pacman -S --noconfirm "${dependencies[@]}"
+pacman -U --noconfirm mingw-w64-ucrt-x86_64-curl-8.8.0-1-any.pkg.tar.zst
+pacman -Syu --noconfirm --ignore=mingw-w64-ucrt-x86_64-curl \
+  base-devel \
+  diffutils \
+  gcc \
+  git \
+  make \
+  mingw-w64-ucrt-x86_64-cmake \
+  mingw-w64-ucrt-x86_64-cppwinrt \
+  mingw-w64-ucrt-x86_64-graphviz \
+  mingw-w64-ucrt-x86_64-miniupnpc \
+  mingw-w64-ucrt-x86_64-nlohmann-json \
+  mingw-w64-ucrt-x86_64-nodejs \
+  mingw-w64-ucrt-x86_64-nsis \
+  mingw-w64-ucrt-x86_64-onevpl \
+  mingw-w64-ucrt-x86_64-openssl \
+  mingw-w64-ucrt-x86_64-opus \
+  mingw-w64-ucrt-x86_64-rust \
+  mingw-w64-ucrt-x86_64-toolchain
 
 # build
 mkdir -p build
+cd build || exit 1
 cmake \
-  -B build \
-  -G Ninja \
-  -S . \
   -DBUILD_DOCS=OFF \
-  -DBUILD_WERROR=ON
-ninja -C build
+  -G "MinGW Makefiles" ..
+mingw32-make -j"$(nproc)"
 
 # skip autobuild
 echo "skip_autobuild=true" >> "$GITHUB_OUTPUT"
