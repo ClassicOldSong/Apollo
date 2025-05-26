03194a6fa3e590e79be1aa920e19241546f93c79
Yukino Song <nutosservice@gmail.com>
2025-05-26 18:08:08 +0800

    UCRT64 C++15 workaround (closes #753)

--- cmake/prep/init.cmake
+++ cmake/prep/init.cmake
@@ -1,4 +1,9 @@
 if (WIN32)
+    if(CMAKE_CXX_COMPILER_ID MATCHES "GNU")
+        if(CMAKE_CXX_COMPILER_VERSION VERSION_GREATER_EQUAL 15.0)
+            add_compile_options($<$<COMPILE_LANGUAGE:CXX>:-Wno-template-body>) # Workaround for WinRT headers
+        endif()
+    endif()
 elseif (APPLE)
 elseif (UNIX)
     include(GNUInstallDirs)
