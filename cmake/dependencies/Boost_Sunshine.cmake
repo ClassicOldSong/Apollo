26f143fad702ce55364fd688e1e0e5ab85e06479
Andreas Nilsson <anil9@kth.se>
2025-05-31 20:44:54 +0200

    Update Boost hash to version 1.88.0

--- cmake/dependencies/Boost_Sunshine.cmake
+++ cmake/dependencies/Boost_Sunshine.cmake
@@ -55,7 +55,7 @@ if(NOT Boost_FOUND)
     # Limit boost to the required libraries only
     set(BOOST_INCLUDE_LIBRARIES ${BOOST_COMPONENTS})
     set(BOOST_URL "https://github.com/boostorg/boost/releases/download/boost-${BOOST_VERSION}/boost-${BOOST_VERSION}-cmake.tar.xz")  # cmake-lint: disable=C0301
-    set(BOOST_HASH "SHA256=7da75f171837577a52bbf217e17f8ea576c7c246e4594d617bfde7fafd408be5")
+    set(BOOST_HASH "SHA256=f48b48390380cfb94a629872346e3a81370dc498896f16019ade727ab72eb1ec")
 
     if(CMAKE_VERSION VERSION_LESS "3.24.0")
         FetchContent_Declare(
