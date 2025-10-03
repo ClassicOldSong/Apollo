f0def369e0d40ca20b1658ae207779bc4f8c4e5d
thesystemcoder168 <204222473+thesystemcoder168@users.noreply.github.com>
2025-10-03 11:22:17 -0700

    Tweaked controller removal script and added better directions to run removal script everytime.
    Tweaked controller removal script for more device manager removals related to controllers and added better directions to run removal script every time and without a reboot.
    
    2 files were changed:
    src_assets\common\assets\web\public\assets\locale\en.json
    src_assets\windows\misc\gamepad\uninstall-existing-controllers-for-alt-controller-enumerating.bat

--- src_assets/windows/misc/gamepad/uninstall-existing-controllers-for-alt-controller-enumerating.bat
+++ src_assets/windows/misc/gamepad/uninstall-existing-controllers-for-alt-controller-enumerating.bat
@@ -1,29 +1,31 @@
 @echo off
 cls
+pushd "%~dp0" 2> null
+pushd "\\%~p0" 2> null
 echo Purpose: This script will clear out the gamepad controllers so that the alternate controller enumerating can be more effective and be less biased by previous controllers.
 echo The will get rid of the unconnected devices for:
 echo "Xbox 360 Controller for Windows"
 echo "HID-compliant game controller"
+echo "USB Input Device"
 echo .....
-echo This should only be run sparingly or when needed when there are unused controller altering the Windows controller enumeration.
-echo 1. Make a Windows Restore Point if concerned about device removal.
+echo This should can be run once or as part of the commands when an application in Apollo is run.
+echo 1. Make a Windows Restore Point if concerned about device removal. (When running for the first time)
 echo 2. Be sure to run this as Administrator
-echo 3. Run this when Apollo is not running.
+echo 3. Run this when Apollo is not running. (Unless part of the sequence before the main Apollo application starts.)
 echo 4. Run this when there are no controllers connected.
-echo 5. This script will prompt to hit any key to run or CTRL-C to stop.
-echo 6. After it is completed, the system must be restarted in order to clear out the older devices.
-echo 7. When the system is rebooted, set the number of preallocated controller to the number that you want in Apollo.
-echo 8. Once clients start joining with controllers, the controller enumeration should be better. This may also get past the 5th+ controller odd numbering situation at least initially.
+echo 5. Make sure the number of alternate controllers that you is filled in Apollo.
+echo 6. Once clients start joining with controllers, the controller enumeration should be better. This should also get past the 5th+ controller odd numbering situation as long as it is run every time the Apollo application is started.
 echo ....
-echo Press CTRL-C to stop script or
-pause
+echo Press CTRL-C to stop script
+rem pause
 powershell.exe -ExecutionPolicy Bypass -File ".\removeGhosts.ps1" -listGhostDevicesOnly
 powershell.exe -ExecutionPolicy Bypass -File ".\removeGhosts.ps1" -narrowbyfriendlyname "Xbox 360 Controller for Windows" -listGhostDevicesOnly
 powershell.exe -ExecutionPolicy Bypass -File ".\removeGhosts.ps1" -narrowbyfriendlyname "HID-compliant game controller" -listGhostDevicesOnly
+powershell.exe -ExecutionPolicy Bypass -File ".\removeGhosts.ps1" -narrowbyfriendlyname "USB Input Device" -listGhostDevicesOnly
 powershell.exe -ExecutionPolicy Bypass -File ".\removeGhosts.ps1" -narrowbyfriendlyname "Xbox 360 Controller for Windows" -force
 powershell.exe -ExecutionPolicy Bypass -File ".\removeGhosts.ps1" -narrowbyfriendlyname "HID-compliant game controller" -force
+powershell.exe -ExecutionPolicy Bypass -File ".\removeGhosts.ps1" -narrowbyfriendlyname "USB Input Device" -force
 echo ....
 echo Done
-echo 6. After it is completed, the system must be restarted in order to clear out the older devices.
-echo 7. When the system is rebooted, set the number of preallocated controller to the number that you want in Apollo.
-echo 8. Once clients start joining with controllers, the controller enumeration should be better. This may also get past the 5th+ controller odd numbering situation at least initially.
+rem pause
+exit 0
